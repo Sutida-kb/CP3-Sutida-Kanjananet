dd506d848addd1e61faef3f83b4db771e228d7a6
Sutida-kb <66106732+Sutida-kb@users.noreply.github.com>
2020-05-29 20:11:33 +0700

    Exercise4_Sutida_K.py

---
+++ Exercise4_Sutida_K.py
@@ -0,0 +1,10 @@
+print("คะแนนสอบนักศึกษาชั้นปีที่ 2 เทอม 1")
+print("--- Your Score ---")
+FE=66.6
+GE=77.7
+ICS=88.8
+CP=99.9
+print("Foundation English               :",FE, "คะแนนที่ได้")
+print("General Business                 :",GE, "คะแนนที่ได้")
+print("Introduction to Computer Systems :",ICS,"คะแนนที่ได้")
+print("Computer Programming             :",CP, "คะแนนที่ได้")
